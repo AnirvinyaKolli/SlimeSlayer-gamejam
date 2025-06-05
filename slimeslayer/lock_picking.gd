d119eb267339355801b017f81861f2ed435aed8f
Shrish-MCVTS <Shrish.kushwaha@mcvts.org>
2025-06-05 09:15:24 -0400

    LockPickFinished
    Need to add the next section code line to the signal for next button.

--- slimeslayer/lock_picking.gd
+++ slimeslayer/lock_picking.gd
@@ -1,26 +1,50 @@
 extends Control
 
+@onready var animation: AnimationPlayer = $LockPick/AnimationPlayer
 @onready var redzone = $Redzone
 @onready var Pin = $Pin
 @onready var ResultLabel = $ResultLabel
 @onready var NextButton = $NextButton
 
-var baseSpeed = 300.0
+var baseSpeed = 600.0
 var speed = baseSpeed
 var direction = 1
 var pinStopped = false
 var level = 1
 var maxLevel = 3
+var in_zone = false
+
 
 func _ready():
-		#updateSpeed()
-		#randomizeZone()
 		NextButton.visible = false
-		#NextButton.pressed.connect(on_next_pressed)
-func _process(delta):
-		if pinStopped:
-			return
+func _physics_process(delta):
+		animation.play("jigglejiggle")
+		if Input.is_action_pressed("ui_accept") and not pinStopped:
+			pinStopped = true
 		Pin.position.x+= direction * speed * delta
 		if Pin.position.x <= -300 or Pin.position.x >= 200:
 			direction*=-1
+		if pinStopped and in_zone:
+			ResultLabel.text = "Success"
+			direction*= 0
+			NextButton.visible = true
+			NextButton.text = "Leave"
+		if pinStopped and not in_zone:
+			ResultLabel.text = "Fail"
+			direction*=0
+			NextButton.visible = true
+			NextButton.text = "Leave"
 		
+
+func _on_redzone_area_shape_entered(area_rid: RID, area: Area2D, area_shape_index: int, local_shape_index: int) -> void:
+	pass # Replace with function body.
+	in_zone = true
+	
+	
+func _on_redzone_area_shape_exited(area_rid: RID, area: Area2D, area_shape_index: int, local_shape_index: int) -> void:
+	pass # Replace with function body.
+	in_zone = false
+	
+	
+func _on_next_button_pressed() -> void:
+	pass # Replace with function body.
